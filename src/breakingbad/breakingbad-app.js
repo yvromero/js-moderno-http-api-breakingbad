a28f601b66a054e11c19d2e00d9c0e46ba434d8e
yvromero <yvonne.romero@uds.edu.py>
2023-05-12 19:45:20 -0400

    First commit

---
+++ src/breakingbad/breakingbad-app.js
@@ -0,0 +1,49 @@
+
+
+/**
+ * @returns {Promise<Object>} quote
+ */
+const fetchQuote = async () => {
+    const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const data = await res.json();
+
+    console.log(data[0]);
+    return data[0];
+}
+
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+
+export const BreakingbadApp = async ( element ) => {
+
+    document.querySelector('#app-title').innerHTML = 'Breaking Bad App';
+    element.innerHTML = 'Loading..';
+    //await fetchQuote();
+
+    const quoteLabel = document.createElement('blockquote');
+    const authoLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerText = 'Next Quote';
+
+    const renderQuote = ( data ) => {
+        quoteLabel.innerHTML = data.quote;
+        authoLabel.innerHTML = data.author;
+        element.replaceChildren( quoteLabel, authoLabel, nextQuoteButton );
+    }
+
+    //Anhadir listener
+    nextQuoteButton.addEventListener('click', async () => {
+        element.innerHTML = 'Loading..';
+        const quote = await fetchQuote();
+        renderQuote( quote );
+
+
+    })
+
+    fetchQuote()
+        .then( renderQuote )
+
+}
